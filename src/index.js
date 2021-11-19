592c0904d901c8ee946ec51e2a80d5ae352a6610
Olli Paloviita <olli@surrogate.tv>
2021-11-19 23:41:59 +0200

    QR code added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="qr-wrapper">
+      <p>Join:</p>
+      <img className="qr" src="/qr.png" alt="qr-code-join" />
+      {/* <a href="https://feel-good-chat.pages.dev">feel-good-chat.pages.dev</a> */}
+    </div>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
